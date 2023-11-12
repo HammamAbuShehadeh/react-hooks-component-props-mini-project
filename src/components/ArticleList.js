912f2d294250820deb06376d999a0b71929f676c
Hammam AbuShehadeh <hammam.y.asmar@gmail.com>
2023-11-13 00:33:27 +0300

    Completed assignment  CLEAN without BONUS

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -11,15 +11,11 @@ function ArticleList({ posts }) {
         date={post.date}
         preview={post.preview}
       />
-      // <>
-      //   <h1 key={post.id}>{post.title}</h1>
-      // </>
     );
   });
   return (
     <main>
       {Articles}
-      {/* <Article posts={posts} />   when I use map function inside Article component  no need to variable Articles in line 6*/}
     </main>
   );
 }
